a9ee12d79911206cf5108e4dd371939cde1104ee
Roberto Arce <rarcemu@gmail.com>
2023-02-26 13:22:38 -0300

    UDP link prod enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: 'backend-impact-prueba-production.up.railway.app/api',
+  url: 'https://backend-impact-prueba-production.up.railway.app/api',
   name:'production environment',
   code: 1004,
 };
